7b73d5a302c5b73df50a7cbe89d8102aa1abb03e
Sille Van Landschoot <info@sillevl.be>
2016-01-21 23:06:13 +0100

    send datawith html form to backend

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 var express = require('express');
 var app = express();
 var exphbs  = require('express-handlebars');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 // handlebars setup and configuration
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -12,6 +18,16 @@ app.use('/static', express.static('public'));
 
 app.get('/', function (req, res) {
   res.render('home');
+});
+
+app.post('/', function(req, res){
+  console.log(req.body);
+  var data = {
+    first: req.body.first,
+    second: req.body.second
+  };
+  console.log(data);
+  res.render('home', data);
 });
 
 var server = app.listen(3000, function () {
