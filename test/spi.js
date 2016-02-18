6b2528d09924a25fc8e39e1976cacfd6c48ed946
Sille Van Landschoot <info@sillevl.be>
2016-02-18 14:25:50 +0100

    test image

--- test/spi.js
+++ test/spi.js
@@ -1,6 +1,17 @@
-var LedPanel = require('../ledpanel').LedPanel;
+var LedPanel = require('../lib/ledpanel').LedPanel;
 
+var getPixels = require("get-pixels")
 var ledpanel = new LedPanel('/dev/spidev0.0');
 
 console.log("*** Ledpanel Demo ***");
-ledpanel.demo();
+//ledpanel.demo();
+
+
+
+getPixels(__dirname + "/../img/testbeeld.png", function(err, pixels) {
+	if(err) throw "could not get pixels: " + err.toString();
+	console.log("got pixels", pixels.shape.slice());	
+//	console.log(pixels.data.slice(0,32*4).toString('hex'));	
+	ledpanel.image(new Buffer(pixels.data));
+});
+
