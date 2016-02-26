fcd5ab1fcb794e8151a83a9adab354eab535b316
Sille Van Landschoot <info@sillevl.be>
2016-02-26 23:54:13 +0100

    basic file uploader

--- src/js/application.js
+++ src/js/application.js
@@ -1,31 +1,43 @@
 var canvas = {
   backgroundColor: '#000000',
   text: "Hello",
+
+  context: $("#canvas")[0].getContext("2d", {alpha: false}),
+
   draw: function(e){
     if(e){
       e.preventDefault(); // prevent page reload
     }
 
-    var first = $("#first").val();
-    var second = $("#second").val();
+    this.context.fillStyle = this.backgroundColor;
+    this.context.fillRect(0,0,96,64);
 
-    var canvas = $("#canvas")[0];
-    var context = canvas.getContext("2d", {alpha: false});
-
-    context.fillStyle = this.backgroundColor;
-    context.fillRect(0,0,96,64);
-
-    context.fillStyle = "#FFFFFF";
-    context.font = "12px Arial";
-    context.fillText(this.text, 20, 20);
+    this.context.fillStyle = "#FFFFFF";
+    this.context.font = "12px Arial";
+    this.context.fillText(this.text, 20, 20);
   },
+
   setBackgroundColor: function(backgroundColor){
     this.backgroundColor = backgroundColor;
     this.draw();
   },
+
   setText: function(text){
     this.text = text;
     this.draw();
+  },
+
+  setImage: function(image){
+    var fr = new FileReader();
+    var that = this;
+    fr.onload = function(e) {
+       var img = new Image();
+       img.onload = function() {
+         that.context.drawImage(img, 0, 0, img.width, img.height, 0, 0, 96, 64);
+       };
+       img.src = e.target.result;
+    };
+    fr.readAsDataURL( image );
   }
 };
 
@@ -35,8 +47,15 @@ var canvas = {
 $(document).ready(function(){
   $("#draw").click(function(e){sendCanvas(e);});
   $("#canvasText").keyup(function(e){canvas.setText($("#canvasText").val());});
+  $("#file")[0].addEventListener("change", uploadImage, false);
   canvas.draw();
 });
+
+var uploadImage = function(){
+  if ( this.files && this.files[0] ) {
+        canvas.setImage(this.files[0]);
+    }
+};
 
 var sendCanvas = function(e){
   e.preventDefault(); // prevent page reload
