169042214fea8ea2e1348a551ddd0b546c6a1b68
Sille Van Landschoot <info@sillevl.be>
2016-03-03 20:52:51 +0100

    change config to use envrionment variabele for production or development port setting

--- app.js
+++ app.js
@@ -25,9 +25,4 @@ app.get('/', function (req, res) {
   res.render('home');
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-});
-
-module.exports = server;
+module.exports = app;
