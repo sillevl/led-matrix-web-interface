7525d7e94a5841f78040e21fc5ab92f5c5897697
Sille Van Landschoot <info@sillevl.be>
2016-03-03 19:48:34 +0100

    removed unused path

--- server.js
+++ server.js
@@ -25,17 +25,6 @@ app.get('/', function (req, res) {
   res.render('home');
 });
 
-app.post('/', function(req, res){
-  var data = {
-    first: req.body.first,
-    second: req.body.second
-  };
-  res.render('home', data);
-});
-
-
-
-
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
